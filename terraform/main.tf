929e8b122eda743756cd0196762e3daa6213b1e4
monish1996 <32761678+monish1996@users.noreply.github.com>
2021-01-19 23:12:49 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "gcs"{
         bucket = "extended-legend-300803_terraform_state"
-        perfix = "state/application-state-file"
+        prefix = "state/application-state-file"
     }
 }
